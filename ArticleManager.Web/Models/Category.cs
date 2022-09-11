092008bafee760c1b127de2bd016737219aded9a
sigged <sderdeyn@gmail.com>
2022-09-11 11:19:11 +0200

    Add Category entity with service

---
+++ ArticleManager.Web/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArticleManager.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name is too long.")]
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+
+    }
+}
---
+++ ArticleManager.Web/Services/Mocks/FakeCategoryService.cs
@@ -0,0 +1,53 @@
+﻿using ArticleManager.Web.Models;
+
+namespace ArticleManager.Web.Services.Mocks
+{
+    public class FakeCategoryService : ICRUDService<Category>
+    {
+        static List<Category> categories = new List<Category>{
+            new Category() { Id = 1, Name = "Category 1", Description = "Description 1" },
+            new Category() { Id = 2, Name = "Category 2", Description = "Description 2" },
+            new Category() { Id = 3, Name = "Category 3", Description = "Description 3" },
+        };
+
+        public Task<Category> Get(int id)
+        {
+            return Task.FromResult(
+                categories.SingleOrDefault(x => x.Id == id)
+            );
+        }
+
+        public Task<IQueryable<Category>> GetAll()
+        {
+            return Task.FromResult(
+                categories.AsQueryable()
+            );
+        }
+
+        public Task Create(Category item)
+        {
+            item.Id = categories.Count() > 0 ? categories.Max(x => x.Id) + 1 : 1;
+            categories.Add(item);
+            return Task.CompletedTask;
+
+        }
+
+        public Task Update(Category item)
+        {
+            var category = categories.SingleOrDefault(x => x.Id == item.Id);
+            if (category == null) throw new ArgumentException("Category not found!");
+            category.Name = item.Name;
+            category.Description = item.Description;
+            return Task.CompletedTask;
+
+        }
+        public Task Delete(int id)
+        {
+            var category = categories.SingleOrDefault(x => x.Id == id);
+            if (category == null) throw new ArgumentException("Category not found!");
+            categories.Remove(category);
+            return Task.CompletedTask;
+        }
+
+    }
+}
