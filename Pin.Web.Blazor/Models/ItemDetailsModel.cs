69fe48a995750e650a1d5e0e1446a94210abae58
sigged <sderdeyn@gmail.com>
2022-09-12 00:49:11 +0200

    Add templated component

---
+++ Pin.Web.Blazor/Models/ItemDetailsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.Web.Blazor.Models
+{
+    public class ItemDetailsModel<TItem>
+    {
+        public string ItemName { get; set; }
+        public TItem Item { get; set; }
+
+    }
+}
