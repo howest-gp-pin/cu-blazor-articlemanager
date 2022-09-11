cf41adbae1d0384d950c7615800b58eef15b1d92
sigged <sderdeyn@gmail.com>
2022-09-11 17:36:57 +0200

    Register HttpClient

--- ArticleManager.Web/Client/Program.cs
+++ ArticleManager.Web/Client/Program.cs
@@ -15,9 +15,9 @@ namespace ArticleManager.Web
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient<ICRUDService<Category>, FakeCategoryService>();
             builder.Services.AddTransient<ICRUDService<Article>, FakeArticleService>();
+            builder.Services.AddSingleton<HttpClient>();
 
             await builder.Build().RunAsync();
         }
