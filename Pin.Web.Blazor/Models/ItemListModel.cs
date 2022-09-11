f815651d98a480313ff702467c980cc3bd0d8988
sigged <sderdeyn@gmail.com>
2022-09-11 22:23:32 +0200

    Create RCL with ItemList Component
    (cherry picked from commit a13a4ab26e457920e1c35e819d30f59b50ca217a)

---
+++ Pin.Web.Blazor/Models/ItemListModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Pin.Web.Blazor.Models
+{
+    public class ItemListModel<T>
+    {
+        public string Title { get; set; }
+        public string ItemName { get; set; }
+        public string[] Headers { get; set; }
+        public T[] Items { get; set; }
+    }
+}
