7475bec5ff0340e29f4f9712ec24473029bdd15c
sigged <sderdeyn@gmail.com>
2022-09-11 15:31:11 +0200

    Create shared library

---
+++ ArticleManager.Shared/ArticleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ArticleManager.Shared
+{
+    public class ArticleDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public CategoryDto Category { get; set; }
+        public string Content { get; set; }
+    }
+}
