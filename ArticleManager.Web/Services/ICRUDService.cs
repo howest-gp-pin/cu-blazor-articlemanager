c577bacfca6070d0b175e585b692c0c1b7b6a46c
sigged <sderdeyn@gmail.com>
2022-09-11 11:19:11 +0200

    Add Category entity with service

---
+++ ArticleManager.Web/Services/ICRUDService.cs
@@ -0,0 +1,13 @@
+﻿namespace ArticleManager.Web.Services
+{
+    public interface ICRUDService<T>
+        where T : class 
+    {
+        Task<IQueryable<T>> GetAll();
+        Task<T> Get(int id);
+        Task Create(T item);
+        Task Update(T item);
+        Task Delete(int id);
+    }
+
+}
