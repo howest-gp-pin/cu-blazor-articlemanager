584f15cc6aa619cd848b7589a81623c2d9d45892
sigged <sderdeyn@gmail.com>
2022-09-11 16:45:59 +0200

    Add API Controllers
    (cherry picked from commit 5c546771ca0e6b904acb639d973284a9c379bea9)

---
+++ ArticleManager.WebApi/Controllers/ArticlesController.cs
@@ -0,0 +1,124 @@
+﻿using ArticleManager.Shared;
+using ArticleManager.WebApi.Data;
+using ArticleManager.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticleManager.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticlesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Articles
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ArticleDto>>> GetArticles()
+        {
+            return await _context.Articles
+                .Include(e => e.Category)
+                .Select(e => new ArticleDto
+                {
+                    Id = e.Id,
+                    Title = e.Title,
+                    Content = e.Content,
+                    Category = new CategoryDto
+                    {
+                        Id = e.Category.Id,
+                        Name = e.Category.Name,
+                        Description = e.Category.Description,
+                    }
+                })
+                .ToListAsync();
+        }
+
+        // GET: api/Articles/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ArticleDto>> GetArticle(int id)
+        {
+            var article = await _context.Articles
+                .Where(e => e.Id == id)
+                .Select(e => new ArticleDto()
+                {
+                    Id = e.Id,
+                    Title = e.Title,
+                    Content = e.Content,
+                    Category = new CategoryDto
+                    {
+                        Id = e.Category.Id,
+                        Name = e.Category.Name,
+                        Description = e.Category.Description,
+                    }
+                })
+                .SingleOrDefaultAsync();
+
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(article);
+        }
+
+        // PUT: api/Articles/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArticle(int id, ArticleDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = _context.Articles.SingleOrDefault(x => x.Id == id);
+                if (entity == null) return NotFound();
+                entity.Title = dto.Title;
+                entity.Content = dto.Content;
+                entity.CategoryId = dto.Category.Id;
+                await _context.SaveChangesAsync();
+                return Ok(entity);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // POST: api/Articles
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ArticleDto>> PostArticle(ArticleDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Article()
+                {
+                    Title = dto.Title,
+                    Content = dto.Content,
+                    CategoryId = dto.Category.Id,
+                };
+                _context.Add(entity);
+                await _context.SaveChangesAsync();
+                dto.Id = entity.Id;
+                return CreatedAtAction(nameof(PostArticle), new { id = entity.Id }, dto);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // DELETE: api/Articles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArticle(int id)
+        {
+            var entity = _context.Articles.SingleOrDefault(x => x.Id == id);
+            if (entity == null) return NotFound();
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+            return Ok(entity);
+        }
+
+        private bool ArticleExists(int id)
+        {
+            return _context.Articles.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ArticleManager.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,110 @@
+﻿using ArticleManager.Shared;
+using ArticleManager.WebApi.Data;
+using ArticleManager.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticleManager.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDto>>> GetCategories()
+        {
+            return await _context.Categories
+                .Select(e => new CategoryDto
+                {
+                    Id = e.Id, 
+                    Name = e.Name,
+                    Description = e.Description
+                })
+                .ToListAsync();
+        }
+
+        // GET: api/Categories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryDto>> GetCategory(int id)
+        {
+            var category = await _context.Categories
+                .Where(x => x.Id == id)
+                .Select(x => new CategoryDto()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description
+                })
+                .SingleOrDefaultAsync();
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+
+        }
+
+        // PUT: api/Categories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(int id, CategoryDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = _context.Categories.SingleOrDefault(x => x.Id == id);
+                if (entity == null) return NotFound();
+                entity.Name = dto.Name;
+                entity.Description = dto.Description;
+                await _context.SaveChangesAsync();
+                return Ok(entity);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // POST: api/Categories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<CategoryDto>> PostCategory(CategoryDto dto)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = new Category()
+                {
+                    Name = dto.Name,
+                    Description = dto.Description,
+                };
+                _context.Add(entity);
+                await _context.SaveChangesAsync();
+                dto.Id = entity.Id;
+                return CreatedAtAction(nameof(PostCategory), new { id = entity.Id }, dto);
+            }
+            return BadRequest(ModelState);
+        }
+
+        // DELETE: api/Categories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var entity = _context.Categories.SingleOrDefault(x => x.Id == id);
+            if (entity == null) return NotFound();
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+            return Ok(entity);
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+}
